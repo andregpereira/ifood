12e24d1bbe02441a44e18ebd1e3617782fe3bad2
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-04 17:32:17 -0300

    Criar UC de update

---
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/domain/usecase/restaurant/RestaurantUpdateImpl.java
@@ -0,0 +1,24 @@
+package com.github.andregpereira.ifood.cadastro.domain.usecase.restaurant;
+
+import com.github.andregpereira.ifood.cadastro.domain.gateway.RestaurantGateway;
+import com.github.andregpereira.ifood.cadastro.domain.model.Restaurant;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Slf4j
+@ApplicationScoped
+public class RestaurantUpdateImpl implements RestaurantUpdate {
+
+    private final RestaurantGateway gateway;
+
+    @Override
+    public Uni<Restaurant> update(UUID id, Restaurant restaurant) {
+        return gateway.update(id, restaurant);
+    }
+
+}
