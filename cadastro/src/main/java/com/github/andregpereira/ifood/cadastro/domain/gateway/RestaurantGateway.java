32b122ab8d63d8bea2caaa113f4eefd84d6b2ced
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-04 17:29:26 -0300

    Refatorar gateway

--- cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/domain/gateway/RestaurantGateway.java
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/domain/gateway/RestaurantGateway.java
@@ -1,19 +1,7 @@
 package com.github.andregpereira.ifood.cadastro.domain.gateway;
 
 import com.github.andregpereira.ifood.cadastro.domain.model.Restaurant;
-import io.smallrye.mutiny.Uni;
 
-import java.util.List;
-import java.util.UUID;
-
-public interface RestaurantGateway {
-
-    Uni<Restaurant> save(Restaurant restaurant);
-
-    Uni<Restaurant> findById(UUID id);
-
-    Uni<List<Restaurant>> findAll();
-
-    Uni<List<Restaurant>> findByName(String name);
+public non-sealed interface RestaurantGateway extends BaseGateway<Restaurant> {
 
 }
