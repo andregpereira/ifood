594e5859304aacb2b43f38cbd127b6dd64c3c85d
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-31 18:30:02 -0300

    Refatorar mapper de data provider de restaurante

--- cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/infra/mapper/RestaurantDataProviderMapper.java
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/infra/mapper/RestaurantDataProviderMapper.java
@@ -7,17 +7,6 @@ import org.mapstruct.*;
 import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.JAKARTA_CDI)
-public interface RestaurantDataProviderMapper {
-
-    Restaurant toModel(RestaurantEntity e);
-
-    RestaurantEntity toEntity(Restaurant r);
-
-    default List<Restaurant> toModelList(List<RestaurantEntity> e) {
-        return e.stream().map(this::toModel).toList();
-    }
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    RestaurantEntity partialUpdate(Restaurant r, @MappingTarget RestaurantEntity e);
+public interface RestaurantDataProviderMapper extends BaseDataProviderMapper<Restaurant, RestaurantEntity> {
 
 }
