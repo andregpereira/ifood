274b85a6773b95da5187da78f7b17615d108d11f
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-31 17:39:01 -0300

    Refatorar código

--- cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/app/resource/RestaurantResource.java
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/app/resource/RestaurantResource.java
@@ -3,6 +3,8 @@ package com.github.andregpereira.ifood.cadastro.app.resource;
 import com.github.andregpereira.ifood.cadastro.app.dto.restaurant.RestaurantCreateDto;
 import com.github.andregpereira.ifood.cadastro.app.dto.restaurant.RestaurantDto;
 import com.github.andregpereira.ifood.cadastro.app.service.RestaurantService;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import io.smallrye.mutiny.Uni;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
@@ -41,37 +43,46 @@ public class RestaurantResource {
     private final UriInfo uriInfo;
 
     private URI getUri(UUID id) {
-        return uriInfo.getAbsolutePathBuilder().path("{id}").build(id);
+        return uriInfo.getAbsolutePathBuilder()
+                .path("{id}")
+                .build(id);
     }
 
     @POST
     public Uni<RestResponse<RestaurantDto>> createRestaurant(RestaurantCreateDto dto) {
-        return restaurantService.create(dto).map(r -> ResponseBuilder.<RestaurantDto>created(getUri(r.id())).entity(
-                r).build());
+        return restaurantService.create(dto)
+                .map(r -> ResponseBuilder.<RestaurantDto>created(getUri(r.id()))
+                        .entity(r)
+                        .build());
     }
 
     @PUT
     @Path("/{id}")
     public Uni<RestResponse<RestaurantDto>> updateRestaurant(@RestPath UUID id, RestaurantCreateDto dto) {
-        return restaurantService.update(id, dto).map(
-                r -> ResponseBuilder.<RestaurantDto>ok().location(getUri(r.id())).build());
+        return restaurantService.update(id, dto)
+                .map(r -> ResponseBuilder.<RestaurantDto>ok()
+                        .location(getUri(r.id()))
+                        .build());
     }
 
     @GET
     @Path("/{id}")
     public Uni<RestResponse<RestaurantDto>> findById(@RestPath UUID id) {
-        return restaurantService.findById(id).map(RestResponse::ok);
+        return restaurantService.findById(id)
+                .map(RestResponse::ok);
     }
 
     @GET
     public Uni<RestResponse<List<RestaurantDto>>> findAll() {
-        return restaurantService.findAll().map(RestResponse::ok);
+        return restaurantService.findAll()
+                .map(RestResponse::ok);
     }
 
     @GET
     @Path("/name")
     public Uni<RestResponse<List<RestaurantDto>>> findByName(@RestQuery String name) {
-        return restaurantService.findByName(name).map(RestResponse::ok);
+        return restaurantService.findByName(name)
+                .map(RestResponse::ok);
     }
 
 }
