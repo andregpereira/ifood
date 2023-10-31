d7110f3fa0edeae6162812a46d831fedd4851238
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-31 18:34:23 -0300

    Criar repository de prato

---
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/infra/repository/DishRepository.java
@@ -0,0 +1,7 @@
+package com.github.andregpereira.ifood.cadastro.infra.repository;
+
+import com.github.andregpereira.ifood.cadastro.infra.entity.DishEntity;
+
+public non-sealed interface DishRepository extends BaseRepository<DishEntity> {
+
+}
---
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/infra/repository/DishRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.github.andregpereira.ifood.cadastro.infra.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class DishRepositoryImpl implements DishRepository {
+
+}
