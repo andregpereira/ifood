c87cfabe15be5d81c854c39af26eba7aa536c46d
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-31 17:32:07 -0300

    Formatar data

--- cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/app/dto/restaurant/RestaurantDto.java
+++ cadastro/src/main/java/com/github/andregpereira/ifood/cadastro/app/dto/restaurant/RestaurantDto.java
@@ -1,6 +1,7 @@
 package com.github.andregpereira.ifood.cadastro.app.dto.restaurant;
 
 import com.github.andregpereira.ifood.cadastro.app.dto.location.LocationDto;
+import jakarta.json.bind.annotation.JsonbDateFormat;
 import jakarta.json.bind.annotation.JsonbPropertyOrder;
 
 import java.time.OffsetDateTime;
@@ -11,8 +12,8 @@ public record RestaurantDto(UUID id,
         String name,
         String owner,
         String cnpj,
-        OffsetDateTime createdDate,
-        OffsetDateTime modifiedDate,
+        @JsonbDateFormat(value = "dd/MM/uuuu - HH:mm") OffsetDateTime createdDate,
+        @JsonbDateFormat(value = "dd/MM/uuuu - HH:mm") OffsetDateTime modifiedDate,
         LocationDto location) {
 
 }
